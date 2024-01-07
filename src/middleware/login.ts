a84e384be8bfc2ee043f5124bb7233f806cda9bd
Salladoretz <salladoretz@yandex.ru>
2024-01-07 17:40:49 +0300

    fix

--- src/middleware/login.ts
+++ src/middleware/login.ts
@@ -6,7 +6,7 @@ export const listnerMiddleware = createListenerMiddleware()
 listnerMiddleware.startListening({
     matcher: authApi.endpoints.login.matchFulfilled,
     effect: async (action, listnerApi) => {
-        listnerApi.cancelActiveListeners
+        listnerApi.cancelActiveListeners()
 
         if (action.payload.token) {
             localStorage.setItem('token', action.payload.token)
